b78390524b981c122971b571f7205f52d4f1e52b
Phan Minh Quoc <120693506+QuocPhan9@users.noreply.github.com>
2025-03-22 08:33:48 +0700

    conn db - set up project

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+import express, {Request, Response} from 'express';
+import cors from 'cors';
+import "dotenv/config";
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.MONGODB_CONN as string);
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/api/test", async (req: Request, res: Response) => {
+    res.json({ message: 'Hello World' });
+});
+
+app.listen(5000, () => {
+    console.log("Server is running on port 5000");
+})
